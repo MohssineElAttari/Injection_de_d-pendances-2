32d8c634908274c6056b2ea119bf802320e1d68e
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-10 09:47:16 +0100

    update interfaxe user dao

--- src/main/java/com/injection/injection_de_dependances/UserDAO.java
+++ src/main/java/com/injection/injection_de_dependances/UserDAO.java
@@ -1,5 +1,8 @@
 package com.injection.injection_de_dependances;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserDAO implements IUserDAO{
     public String[] getUsers(){
         String[] users= {"mohssine","elattari"};
