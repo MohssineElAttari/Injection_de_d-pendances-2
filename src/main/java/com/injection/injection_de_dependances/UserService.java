320be58336dba8d17fe67e69678f3888926732b6
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-10 09:46:39 +0100

    update class implementation user service

--- src/main/java/com/injection/injection_de_dependances/UserService.java
+++ src/main/java/com/injection/injection_de_dependances/UserService.java
@@ -1,18 +1,17 @@
 package com.injection.injection_de_dependances;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
 public class UserService implements IUserService{
+    @Autowired
     private UserDAO userDAO ;
     public String getFullName(){
         String[] userInfo = userDAO.getUsers();
         return userInfo[0]+" - "+userInfo[1];
     }
 
-    public void setUserDAO(UserDAO userDAO) {
-        this.userDAO = userDAO;
-    }
-
-    public UserDAO getUserDAO(){
-        return this.userDAO;
-    }
 
 }
