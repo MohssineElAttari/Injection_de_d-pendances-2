16e1be90c7ba7060725fcbfe406c72d4b679a3c0
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-10 09:48:03 +0100

    update in class main

--- src/main/java/com/injection/injection_de_dependances/InjectionDeDependancesApplication.java
+++ src/main/java/com/injection/injection_de_dependances/InjectionDeDependancesApplication.java
@@ -1,16 +1,21 @@
 package com.injection.injection_de_dependances;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class InjectionDeDependancesApplication {
+    @Autowired
+    private IUserService iUserService;
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext context =
         SpringApplication.run(InjectionDeDependancesApplication.class, args);
-        UserService userService = new UserService();
-        userService.setUserDAO(new UserDAO());
-        String name = userService.getFullName();
+        UserService user =context.getBean(UserService.class);
+        String name = user.getFullName();
 
         System.out.println("####################################");
         System.out.println(name);
